79bda4220821c55a85b9ece7fdd51bf1508885b9
kelley <kelleylsharp@gmail.com>
2019-04-23 20:43:42 -0700

    update npm

--- src/components/Pet/index.js
+++ src/components/Pet/index.js
@@ -2,7 +2,27 @@ import React, { Component } from 'react';
 
 class Pet extends Component {
   render() {
-    return <div>{this.props.breed}</div>;
+    const { name, animal, breed, media, location } = this.props;
+
+    let photos = [];
+
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo['@size'] === 'pn');
+    }
+
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
+      </div>
+    );
   }
 }
 
